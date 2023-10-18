63ccfae94ce7f05c49e4ae940f4978d247e27ac8
Jonatan Fontanez <jonfon01@mit.edu>
2023-10-17 20:43:05 -0400

    Add app.ts, responses.ts, and routes.ts from A4

--- server/app.ts
+++ server/app.ts
@@ -2,9 +2,17 @@ import FriendConcept from "./concepts/friend";
 import PostConcept from "./concepts/post";
 import UserConcept from "./concepts/user";
 import WebSessionConcept from "./concepts/websession";
+import CommentConcept from "./concepts/comment";
+import LikeConcept from "./concepts/likes";
+import TrustConcept from "./concepts/trusts";
+import KarmaConcept from "./concepts/karma";
 
 // App Definition using concepts
 export const WebSession = new WebSessionConcept();
 export const User = new UserConcept();
 export const Post = new PostConcept();
 export const Friend = new FriendConcept();
+export const Comment = new CommentConcept();
+export const Like = new LikeConcept();
+export const Trust = new TrustConcept();
+export const Karma = new KarmaConcept();
--- server/routes.ts
+++ server/routes.ts
@@ -2,11 +2,13 @@ import { ObjectId } from "mongodb";
 
 import { Router, getExpressRouter } from "./framework/router";
 
-import { Friend, Post, User, WebSession } from "./app";
+import { Friend, Post, User, WebSession, Comment, Like, Trust, Karma } from "./app";
 import { PostDoc, PostOptions } from "./concepts/post";
+import { CommentDoc } from "./concepts/comment";
 import { UserDoc } from "./concepts/user";
 import { WebSessionDoc } from "./concepts/websession";
 import Responses from "./responses";
+import { TrustType } from "./concepts/trusts";
 
 class Routes {
   @Router.get("/session")
@@ -23,6 +25,11 @@ class Routes {
   @Router.get("/users/:username")
   async getUser(username: string) {
     return await User.getUserByUsername(username);
+  }
+
+  @Router.get("/users/:username/karma")
+  async getKarma(username: string) {
+    return await Karma.getKarma((await User.getUserByUsername(username))._id);
   }
 
   @Router.post("/users")
@@ -88,6 +95,195 @@ class Routes {
     const user = WebSession.getUser(session);
     await Post.isAuthor(user, _id);
     return Post.delete(_id);
+  }
+
+  @Router.get("/posts/:_id/likes")
+  async getPostLikes(_id: ObjectId) {
+    await Post.assertPostExists(_id);
+    return Like.getLikes(_id);
+  }
+
+  @Router.patch("/posts/:_id/like")
+  async addPostLike(session: WebSessionDoc, _id: ObjectId) {
+    await Post.assertPostExists(_id);
+    const user = WebSession.getUser(session);
+    return Like.like(user, _id);
+  }
+
+  @Router.patch("/posts/:_id/dislike")
+  async addPostDislike(session: WebSessionDoc, _id: ObjectId) {
+    await Post.assertPostExists(_id);
+    const user = WebSession.getUser(session);
+    return Like.dislike(user, _id);
+  }
+
+  @Router.patch("/posts/:_id/neutral_like")
+  async neutralLikePost(session: WebSessionDoc, _id: ObjectId) {
+    await Post.assertPostExists(_id);
+    const user = WebSession.getUser(session);
+    return Like.neutralize(user, _id);
+  }
+
+  @Router.get("/posts/:_id/trusts")
+  async getPostTrusts(_id: ObjectId) {
+    await Post.assertPostExists(_id);
+    return Trust.getTrusts(_id);
+  }
+
+  @Router.patch("/posts/:_id/trust")
+  async addPostTrust(session: WebSessionDoc, _id: ObjectId) {
+    await Post.assertPostExists(_id);
+    const author = (await Post.posts.readOne({ _id }))?.author;
+    const user = WebSession.getUser(session);
+    if (author) {
+      const trust = await Trust.trust(user, _id);
+      const karma = await Karma.increaseKarma(author, trust.typeRemoved == TrustType.Mistrust);
+      return { trustInfo: trust, karmaInfo: karma };
+    }
+  }
+
+  @Router.patch("/posts/:_id/mistrust")
+  async addPostMistrust(session: WebSessionDoc, _id: ObjectId) {
+    await Post.assertPostExists(_id);
+    const author = (await Post.posts.readOne({ _id }))?.author;
+    const user = WebSession.getUser(session);
+    if (author) {
+      const mistrust = await Trust.mistrust(user, _id);
+      const karma = await Karma.decreaseKarma(author, mistrust.typeRemoved == TrustType.Trust);
+      return { trustInfo: mistrust, karmaInfo: karma };
+    }
+  }
+
+  @Router.patch("/posts/:_id/neutral_trust")
+  async neutralTrustPost(session: WebSessionDoc, _id: ObjectId) {
+    await Post.assertPostExists(_id);
+    const author = (await Post.posts.readOne({ _id }))?.author;
+    const user = WebSession.getUser(session);
+    const trustInfo = await Trust.neutralize(user, _id);
+    if (author) {
+      let karma;
+      const typeRemoved = trustInfo.typeRemoved;
+      if (typeRemoved == TrustType.Trust) {
+        karma = await Karma.decreaseKarma(author, false);
+      } else if (typeRemoved == TrustType.Mistrust) {
+        karma = await Karma.increaseKarma(author, false);
+      } else {
+        console.log(author);
+        karma = { msg: "Karma not modified!", karma: await Karma.karma.readOne({ user: author }) };
+      }
+      return { trustInfo: trustInfo, karmaInfo: karma };
+    }
+  }
+
+  @Router.get("/comments")
+  async getComments(author?: string) {
+    let comments;
+    if (author) {
+      const id = (await User.getUserByUsername(author))._id;
+      comments = await Comment.getByAuthor(id);
+    } else {
+      comments = await Comment.getComments({});
+    }
+    return Responses.comments(comments);
+  }
+
+  @Router.post("/comments")
+  async createComment(session: WebSessionDoc, content: string, target: ObjectId) {
+    const user = WebSession.getUser(session);
+    await Post.assertPostExists(target);
+    const created = await Comment.create(user, target, content);
+    return { msg: created.msg, comment: await Responses.comment(created.comment) };
+  }
+
+  @Router.patch("/comments/:_id")
+  async updateComment(session: WebSessionDoc, _id: ObjectId, update: Partial<CommentDoc>) {
+    const user = WebSession.getUser(session);
+    await Comment.isAuthor(user, _id);
+    return await Comment.update(_id, update);
+  }
+
+  @Router.delete("/comments/:_id")
+  async deleteComment(session: WebSessionDoc, _id: ObjectId) {
+    const user = WebSession.getUser(session);
+    await Comment.isAuthor(user, _id);
+    return Comment.delete(_id);
+  }
+
+  @Router.get("/comments/:_id/likes")
+  async getCommentLikes(_id: ObjectId) {
+    await Comment.assertCommentExists(_id);
+    return Like.getLikes(_id);
+  }
+
+  @Router.patch("/comments/:_id/like")
+  async addCommentLike(session: WebSessionDoc, _id: ObjectId) {
+    await Comment.assertCommentExists(_id);
+    const user = WebSession.getUser(session);
+    return Like.like(user, _id);
+  }
+
+  @Router.patch("/comments/:_id/dislike")
+  async addCommentDislike(session: WebSessionDoc, _id: ObjectId) {
+    await Comment.assertCommentExists(_id);
+    const user = WebSession.getUser(session);
+    return Like.dislike(user, _id);
+  }
+
+  @Router.patch("/comments/:_id/neutral_like")
+  async neutralLikeComment(session: WebSessionDoc, _id: ObjectId) {
+    await Comment.assertCommentExists(_id);
+    const user = WebSession.getUser(session);
+    return Like.neutralize(user, _id);
+  }
+
+  @Router.get("/comments/:_id/trusts")
+  async getCommentTrusts(_id: ObjectId) {
+    await Comment.assertCommentExists(_id);
+    return Trust.getTrusts(_id);
+  }
+
+  @Router.patch("/comments/:_id/trust")
+  async addCommentTrust(session: WebSessionDoc, _id: ObjectId) {
+    await Comment.assertCommentExists(_id);
+    const author = (await Comment.comments.readOne({ _id }))?.author;
+    const user = WebSession.getUser(session);
+    if (author) {
+      const trust = await Trust.trust(user, _id);
+      const karma = await Karma.increaseKarma(author, trust.typeRemoved == TrustType.Mistrust);
+      return { trustInfo: trust, karmaInfo: karma };
+    }
+  }
+
+  @Router.patch("/comments/:_id/mistrust")
+  async addCommentMistrusts(session: WebSessionDoc, _id: ObjectId) {
+    await Comment.assertCommentExists(_id);
+    const author = (await Comment.comments.readOne({ _id }))?.author;
+    const user = WebSession.getUser(session);
+    if (author) {
+      const mistrust = await Trust.mistrust(user, _id);
+      const karma = await Karma.decreaseKarma(author, mistrust.typeRemoved == TrustType.Trust);
+      return { trustInfo: mistrust, karmaInfo: karma };
+    }
+  }
+
+  @Router.patch("/comments/:_id/neutral_trust")
+  async neutralTrustComment(session: WebSessionDoc, _id: ObjectId) {
+    await Comment.assertCommentExists(_id);
+    const author = (await Post.posts.readOne({ _id }))?.author;
+    const user = WebSession.getUser(session);
+    const trustInfo = await Trust.neutralize(user, _id);
+    if (author) {
+      let karma;
+      const typeRemoved = trustInfo.typeRemoved;
+      if (typeRemoved == TrustType.Trust) {
+        karma = await Karma.decreaseKarma(author, false);
+      } else if (typeRemoved == TrustType.Mistrust) {
+        karma = await Karma.increaseKarma(author, false);
+      } else {
+        karma = { msg: "Karma not modified!", karma: await Karma.karma.readOne({ user: author }) };
+      }
+      return { trustInfo: trustInfo, karmaInfo: karma };
+    }
   }
 
   @Router.get("/friends")
