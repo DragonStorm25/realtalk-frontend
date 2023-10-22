dd07070a03dc0ddaedf432cac7729f2cfa470586
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-10-22 19:55:13 -0400

    Add basic function to get likes given id

--- client/components/Likes/LikeComponent.vue
+++ client/components/Likes/LikeComponent.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
+import { fetchy } from "../../utils/fetchy";
+
 const props = defineProps(["post_id"]);
+
+async function getLikes() {
+  let likeResults;
+  try {
+    likeResults = await fetchy(`/api/posts/${props.post_id}/likes`, "GET");
+  } catch (_) {
+    return;
+  }
+}
 </script>
 
 <template>
