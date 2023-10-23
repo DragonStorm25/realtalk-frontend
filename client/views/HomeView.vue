fe5e507eb289041d24e527a49e95f0db5b642abe
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-10-22 20:37:29 -0400

    Add split view

--- client/views/HomeView.vue
+++ client/views/HomeView.vue
@@ -8,17 +8,39 @@ const { currentUsername, isLoggedIn } = storeToRefs(useUserStore());
 
 <template>
   <main>
-    <h1>Home Page</h1>
-    <section>
-      <h1 v-if="isLoggedIn">Welcome {{ currentUsername }}!</h1>
-      <h1 v-else>Please login!</h1>
-    </section>
-    <PostListComponent />
+    <div class="split left">
+      <h1>Home Page</h1>
+      <section>
+        <h1 v-if="isLoggedIn">Welcome {{ currentUsername }}!</h1>
+        <h1 v-else>Please login!</h1>
+      </section>
+    </div>
+    <div class="split right">
+      <PostListComponent />
+    </div>
   </main>
 </template>
 
 <style scoped>
 h1 {
   text-align: center;
+}
+
+/* Split the screen in half */
+.split {
+  height: 100%;
+  position: fixed;
+}
+
+/* Control the left side */
+.left {
+  width: 30%;
+  left: 0;
+}
+
+/* Control the right side */
+.right {
+  width: 70%;
+  right: 0;
 }
 </style>
