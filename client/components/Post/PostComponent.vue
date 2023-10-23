841e292e1b6a4a16e53a150dae88ed404412c3ce
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-10-22 20:38:42 -0400

    Organize buttons

--- client/components/Post/PostComponent.vue
+++ client/components/Post/PostComponent.vue
@@ -24,8 +24,10 @@ const deletePost = async () => {
   <p class="author">{{ props.post.author }}</p>
   <p>{{ props.post.content }}</p>
   <div class="base">
-    <LikeComponent :post_id="props.post._id" />
-    <TrustComponent :post_id="props.post._id" />
+    <div class="base">
+      <LikeComponent :post_id="props.post._id" />
+      <TrustComponent :post_id="props.post._id" />
+    </div>
     <menu v-if="props.post.author == currentUsername">
       <li><button class="btn-small pure-button" @click="emit('editPost', props.post._id)">Edit</button></li>
       <li><button class="button-error btn-small pure-button" @click="deletePost">Delete</button></li>
