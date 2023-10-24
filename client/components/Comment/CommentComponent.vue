a12ba4d7ff774e0a94f0d9fa7cde8421a5d0bec4
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-10-24 19:49:14 -0400

    Add basic tags from LikeComponent

---
+++ client/components/Comment/CommentComponent.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { fetchy } from "../../utils/fetchy";
+import { onBeforeMount, ref } from "vue";
+
+const props = defineProps(["post_id"]);
+</script>
+
+<template>
+  <div class="likes-box">
+    <button class="pure-button" @click="likePost">Likes: {{ likes.likes }}</button>
+    <button class="pure-button" @click="dislikePost">Dislikes: {{ likes.dislikes }}</button>
+  </div>
+</template>
+
+<style scoped>
+button {
+  margin: 0em;
+  margin-left: 1em;
+}
+
+.likes-box {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
