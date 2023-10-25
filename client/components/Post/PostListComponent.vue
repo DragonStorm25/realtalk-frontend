cc9c68999900fddf3535c6cf701712edd81c50ff
Jonatan Fontanez <jonfon01@mit.edu>
2023-10-25 19:21:43 -0400

    Wrap entire html in scroll

--- client/components/Post/PostListComponent.vue
+++ client/components/Post/PostListComponent.vue
@@ -38,23 +38,25 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-  <section v-if="isLoggedIn">
-    <h2>Create a post:</h2>
-    <CreatePostForm @refreshPosts="getPosts" />
-  </section>
-  <div class="row">
-    <h2 v-if="!searchAuthor">Posts:</h2>
-    <h2 v-else>Posts by {{ searchAuthor }}:</h2>
-    <SearchPostForm @getPostsByAuthor="getPosts" />
+  <div class="scroll">
+    <section v-if="isLoggedIn">
+      <h2>Create a post:</h2>
+      <CreatePostForm @refreshPosts="getPosts" />
+    </section>
+    <div class="row">
+      <h2 v-if="!searchAuthor">Posts:</h2>
+      <h2 v-else>Posts by {{ searchAuthor }}:</h2>
+      <SearchPostForm @getPostsByAuthor="getPosts" />
+    </div>
+    <section class="posts" v-if="loaded && posts.length !== 0">
+      <article v-for="post in posts" :key="post._id">
+        <PostComponent v-if="editing !== post._id" :post="post" @refreshPosts="getPosts" @editPost="updateEditing" />
+        <EditPostForm v-else :post="post" @refreshPosts="getPosts" @editPost="updateEditing" />
+      </article>
+    </section>
+    <p v-else-if="loaded">No posts found</p>
+    <p v-else>Loading...</p>
   </div>
-  <section class="posts" v-if="loaded && posts.length !== 0">
-    <article v-for="post in posts" :key="post._id">
-      <PostComponent v-if="editing !== post._id" :post="post" @refreshPosts="getPosts" @editPost="updateEditing" />
-      <EditPostForm v-else :post="post" @refreshPosts="getPosts" @editPost="updateEditing" />
-    </article>
-  </section>
-  <p v-else-if="loaded">No posts found</p>
-  <p v-else>Loading...</p>
 </template>
 
 <style scoped>
@@ -81,8 +83,11 @@ article {
 }
 
 .posts {
-  height: 50%;
   padding: 1em;
+}
+
+.scroll {
+  height: 90%;
   overflow-y: auto;
 }
 
