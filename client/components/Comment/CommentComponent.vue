52f946c1f53f7c07a0624003b44cecc19ff111e0
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-10-24 23:01:31 -0400

    Add initial edit and delete buttons to comments

--- client/components/Comment/CommentComponent.vue
+++ client/components/Comment/CommentComponent.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
 import LikeComponent from "../Likes/LikeComponent.vue";
 import TrustComponent from "../Trusts/TrustComponent.vue";
+import { useUserStore } from "@/stores/user";
 import { formatDate } from "@/utils/formatDate";
+import { storeToRefs } from "pinia";
+import { fetchy } from "../../utils/fetchy";
 
 const props = defineProps(["comment"]);
+const emit = defineEmits(["editComment", "refreshComments"]);
+
+const { currentUsername } = storeToRefs(useUserStore());
+
+const deleteComment = async () => {
+  try {
+    await fetchy(`/api/comments/${props.comment._id}`, "DELETE");
+  } catch {
+    return;
+  }
+  emit("refreshComments");
+};
 </script>
 
 <template>
@@ -15,6 +30,10 @@ const props = defineProps(["comment"]);
         <LikeComponent :target_id="props.comment._id" target_type="comment" />
         <TrustComponent :target_id="props.comment._id" target_type="comment" />
       </div>
+      <menu v-if="props.comment.author == currentUsername">
+        <li><button class="btn-small pure-button" @click="emit('editComment', props.comment._id)">Edit</button></li>
+        <li><button class="button-error btn-small pure-button" @click="deleteComment">Delete</button></li>
+      </menu>
       <article class="timestamp">
         <p v-if="props.comment.dateCreated !== props.comment.dateUpdated">Edited on: {{ formatDate(props.comment.dateUpdated) }}</p>
         <p v-else>Created on: {{ formatDate(props.comment.dateCreated) }}</p>
