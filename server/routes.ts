c911d30c67f3a513da87c7b80cf567ca5ea238dc
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-10-24 23:03:40 -0400

    Fix comment trust and mistrust not showing confirmation

--- server/routes.ts
+++ server/routes.ts
@@ -257,7 +257,7 @@ class Routes {
     if (author) {
       const trust = await Trust.trust(user, _id);
       const karma = await Karma.increaseKarma(author, trust.typeRemoved == TrustType.Mistrust);
-      return { trustInfo: trust, karmaInfo: karma };
+      return { msg: "Trust successfully applied and karma successfully increased!", trustInfo: trust, karmaInfo: karma };
     }
   }
 
@@ -269,7 +269,7 @@ class Routes {
     if (author) {
       const mistrust = await Trust.mistrust(user, _id);
       const karma = await Karma.decreaseKarma(author, mistrust.typeRemoved == TrustType.Trust);
-      return { trustInfo: mistrust, karmaInfo: karma };
+      return { msg: "Mistrust successfully applied and karma successfully decreased!", trustInfo: mistrust, karmaInfo: karma };
     }
   }
 
