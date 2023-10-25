25fe1edf342ede44ca635783151c9b8f703f706c
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-10-24 22:18:00 -0400

    Add basic create comment toggle component

---
+++ client/components/Comment/CreateCommentToggle.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <button class="pure-button">Comment</button>
+</template>
+
+<style scoped>
+form {
+  background-color: var(--base-bg);
+  border-radius: 1em;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
+  padding: 1em;
+}
+
+textarea {
+  font-family: inherit;
+  font-size: inherit;
+  height: 6em;
+  padding: 0.5em;
+  border-radius: 4px;
+  resize: none;
+}
+</style>
