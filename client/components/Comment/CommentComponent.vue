29b1f10d513f2313d7ec9fa368be90a4e5e00bcc
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-10-24 21:06:41 -0400

    Add timestamp to comments

--- client/components/Comment/CommentComponent.vue
+++ client/components/Comment/CommentComponent.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { formatDate } from "@/utils/formatDate";
+
 const props = defineProps(["comment"]);
 </script>
 
@@ -6,7 +8,12 @@ const props = defineProps(["comment"]);
   <div class="comment-box">
     <p class="author">Author: {{ props.comment.author }}</p>
     <p>Content: {{ props.comment.content }}</p>
-    <div class="base"></div>
+    <div class="base">
+      <article class="timestamp">
+        <p v-if="props.comment.dateCreated !== props.comment.dateUpdated">Edited on: {{ formatDate(props.comment.dateUpdated) }}</p>
+        <p v-else>Created on: {{ formatDate(props.comment.dateCreated) }}</p>
+      </article>
+    </div>
   </div>
 </template>
 
