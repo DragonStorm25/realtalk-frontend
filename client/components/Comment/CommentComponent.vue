aaabeb0de35c021bf70a95b02da3e35817dbc852
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-10-24 21:07:18 -0400

    Add more CSS from posts

--- client/components/Comment/CommentComponent.vue
+++ client/components/Comment/CommentComponent.vue
@@ -27,9 +27,20 @@ p {
   font-size: 1.2em;
 }
 
+.timestamp {
+  display: flex;
+  justify-content: flex-end;
+  font-size: 0.9em;
+  font-style: italic;
+}
+
 .base {
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+
+.base article:only-child {
+  margin-left: auto;
 }
 </style>
