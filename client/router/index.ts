271e32c5ce522f0db81a85631304d98447b4539f
Jonatan Fontanez <jonfon01@mit.edu>
2023-10-26 21:27:01 -0400

    Add credits to index

--- client/router/index.ts
+++ client/router/index.ts
@@ -6,6 +6,7 @@ import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/LoginView.vue";
 import NotFoundView from "../views/NotFoundView.vue";
 import SettingView from "../views/SettingView.vue";
+import CreditView from "../views/CreditView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/",
       name: "Home",
       component: HomeView,
+    },
+    {
+      path: "/credits",
+      name: "Credit",
+      component: CreditView,
     },
     {
       path: "/setting",
