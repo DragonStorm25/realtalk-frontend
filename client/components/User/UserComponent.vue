538eb472700c2638d5610c033e4637ec578a30df
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-10-27 17:20:38 -0400

    Add manual user override to UserComponent

--- client/components/User/UserComponent.vue
+++ client/components/User/UserComponent.vue
@@ -1,18 +1,47 @@
 <script setup lang="ts">
 import { useUserStore } from "@/stores/user";
 import { storeToRefs } from "pinia";
+import { fetchy } from "@/utils/fetchy";
+import { onBeforeMount, ref } from "vue";
 
 const { currentUsername, isLoggedIn, currentKarma } = storeToRefs(useUserStore());
+
+const props = defineProps(["overrideUsername"]);
+let override = ref(false);
+let overrideKarma = ref(0);
+
+async function getOverridenUserKarma() {
+  let karmaRequest;
+  try {
+    karmaRequest = await fetchy(`/api/users/${props.overrideUsername}/karma`, "GET");
+  } catch (_) {
+    return;
+  }
+  overrideKarma.value = karmaRequest;
+}
+
+onBeforeMount(async () => {
+  if (props.overrideUsername) {
+    override.value = true;
+    await getOverridenUserKarma();
+  }
+});
 </script>
 
 <template>
   <div class="profile-wrapper">
-    <div v-if="isLoggedIn" class="logged-in">
-      <p class="username">{{ currentUsername }}</p>
-      <p class="karma">{{ currentKarma > 0 ? "+" : currentKarma < 0 ? "-" : "" }}{{ Math.abs(currentKarma) }} realness</p>
+    <div v-if="!override">
+      <div v-if="isLoggedIn" class="logged-in">
+        <p class="username">{{ currentUsername }}</p>
+        <p class="karma">{{ currentKarma > 0 ? "+" : currentKarma < 0 ? "-" : "" }}{{ Math.abs(currentKarma) }} realness</p>
+      </div>
+      <div v-else>
+        <p class="username logged-out">Not logged in!</p>
+      </div>
     </div>
     <div v-else>
-      <p class="username logged-out">Not logged in!</p>
+      <p class="username">{{ props.overrideUsername }}</p>
+      <p class="karma">{{ overrideKarma > 0 ? "+" : overrideKarma < 0 ? "-" : "" }}{{ Math.abs(overrideKarma) }} realness</p>
     </div>
   </div>
 </template>
