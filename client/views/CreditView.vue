c08bfa8ac0c3ae20385c629d90491878bff86e77
Jonatan Fontanez <jonfon01@mit.edu>
2023-10-26 21:33:25 -0400

    Make background darker-bg

--- client/views/CreditView.vue
+++ client/views/CreditView.vue
@@ -1,10 +1,15 @@
 <template>
-  <main class="column">
+  <main class="credits">
     <a href="https://www.vecteezy.com/free-vector/like-dislike">Like Dislike Vectors by Vecteezy</a>
   </main>
 </template>
 
 <style scoped>
+.credits {
+  background: var(--darker-bg);
+  height: 100vh;
+}
+
 a {
   background: var(--darker-bg);
   color: var(--font-color);
