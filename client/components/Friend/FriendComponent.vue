71f62c4811ae32bf46287e3f75f605e7124b643a
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-10-27 14:37:16 -0400

    Create FriendComponent.vue

---
+++ client/components/Friend/FriendComponent.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useUserStore } from "@/stores/user";
+import { storeToRefs } from "pinia";
+
+const props = defineProps(["username"]);
+</script>
+
+<template>
+  <div class="friend-wrapper">
+    <p class="username">{{ props.username }}</p>
+  </div>
+</template>
+
+<style scoped>
+.username {
+  font-weight: bold;
+  font-size: 1.2em;
+  border-bottom-style: solid;
+  border-bottom-color: var(--underline-color);
+}
+
+.friend-wrapper {
+  background-color: var(--base-bg);
+  border-radius: 1em;
+  padding: 1em;
+}
+</style>
