31d375d96ef8b1c18df70e7279fb264d74ac2008
Jonatan Fontanez <jonfon01@mit.edu>
2023-10-26 21:26:02 -0400

    Add page for credit

---
+++ client/views/CreditView.vue
@@ -0,0 +1,12 @@
+<template>
+  <main class="column">
+    <a href="https://www.vecteezy.com/free-vector/like-dislike">Like Dislike Vectors by Vecteezy</a>
+  </main>
+</template>
+
+<style scoped>
+a {
+  background: var(--darker-bg);
+  color: var(--font-color);
+}
+</style>
